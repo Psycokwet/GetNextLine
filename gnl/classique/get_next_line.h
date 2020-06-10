75fcd042479e52a03947bca0e38b5f7712d6bf6e
Psycokwet <socarboni@gmail.com>
2020-06-10 20:49:06 +0200

    update guac

--- gnl/classique/get_next_line.h
+++ gnl/classique/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: scarboni <scarboni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/05 11:14:23 by scarboni          #+#    #+#             */
-/*   Updated: 2020/06/10 20:47:47 by scarboni         ###   ########.fr       */
+/*   Updated: 2020/06/10 20:48:56 by scarboni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,7 @@ typedef struct	s_fd_read_wip
 	size_t	    	size;
 	char	        *line_wip;
 }				t_fd_read_wip, *pt_fd_read_wip;
+typedef t_fd_read_wip, *pt_fd_read_wip;
 
 int	get_next_line(int fd, char **line);
 int	ft_strchr(const char *s, int c, size_t * indice);
