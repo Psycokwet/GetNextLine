20b216594c1b6b4fc1504617587612d6ce0956c8
Psycokwet <socarboni@gmail.com>
2020-06-12 19:32:18 +0200

    update guac

--- gnl/classique/get_next_line_utils.c
+++ gnl/classique/get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: scarboni <scarboni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/05 10:38:18 by scarboni          #+#    #+#             */
-/*   Updated: 2020/06/12 19:25:08 by scarboni         ###   ########.fr       */
+/*   Updated: 2020/06/12 19:32:00 by scarboni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,4 +58,32 @@ size_t		ft_strlcpy(char *dst, const char *src, size_t dstsize)
 	while (src[i])
 		i++;
 	return (i);
+}
+
+int	get_next_line(int fd, char **line){
+	static t_fd_read_wip *current_wip;
+	int cut_line_n_ret;
+	int return_value;
+	if(!line)
+		return -1;
+	if(*line)
+		free(*line);
+	current_wip = get_current_wip(current_wip, fd);
+	if(!current_wip)
+		return -1;
+	if(current_wip->line_wip){
+		cut_line_n_ret = cut_line_n(line, current_wip);
+		if(cut_line_n_ret != 2){
+            return cut_line_n_ret;
+        }
+	}
+	return_value = read_full_line(current_wip, line);
+	if(return_value != 1){
+		if(current_wip){
+			if(current_wip->line_wip)
+				free(current_wip->line_wip);
+			free(current_wip);
+		}
+	}
+	return return_value;
 }
