ba5a8dadc1cededa82b4760c34b467881406803c
Psycokwet <socarboni@gmail.com>
2020-06-23 22:27:29 +0200

    update guac

--- gnl/classique/get_next_line.h
+++ gnl/classique/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: scarboni <scarboni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/05 11:14:23 by scarboni          #+#    #+#             */
-/*   Updated: 2020/06/23 22:02:38 by scarboni         ###   ########.fr       */
+/*   Updated: 2020/06/23 22:27:17 by scarboni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,7 +37,6 @@ typedef struct	s_fd_read_wip
 # define ENDL_FOUND			1
 # define ENDL_NOT_FOUND		0
 
-
 int				ft_strchr(const char *s, int c, ssize_t *indice);
 char			*ft_strdup(const char *src);
 size_t			ft_strlcpy(char *dst, const char *src, size_t dstsize);
@@ -45,7 +44,8 @@ size_t			ft_strlen(const char *s);
 int				cut_line_n(char **line, t_fd_read_wip *fd_wip);
 int				append_buffer(t_fd_read_wip *fd_wip, char *buffer,
 ssize_t ret_read);
-int				read_full_line(t_fd_read_wip *fd_wip, char **line, char *buffer);
+int				read_full_line(t_fd_read_wip *fd_wip, char **line,
+				char *buffer);
 void			set_current_wip(t_fd_read_wip *current_wip, int fd);
 int				get_next_line(int fd, char **line);
 
